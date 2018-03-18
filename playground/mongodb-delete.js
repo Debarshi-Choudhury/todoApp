d44b5cabb8db84af641b66c26791e47f9ba7b43b
Debarshi-Choudhury <debarshi.choudhury1@gmail.com>
2018-03-19 02:16:02 +0530

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+const {MongoClient,ObjectID}=require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+	if(err){
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+	//deleteMany
+	// db.collection('Todos').deleteMany({text:'eat lunch'}).then((result)=>{
+	// 	console.log(result);
+	// });
+
+
+	//deleteOne
+	// db.collection('Todos').deleteOne({text:'eat lunch'}).then((result)=>{
+	// 	console.log(result);
+	// });
+
+
+	//findOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed:false}).then((result)=>{
+	// 	console.log(result);
+	// });
+
+	// db.collection('Users').deleteMany({name:'Debarshi Choudhury'}).then((result)=>{
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndDelete({_id:new ObjectID('5aac34707c8a1b0a3e877d7a')}).then((result)=>{
+		console.log(JSON.stringify(result,	undefined, 2));
+	});
+
+	//db.close();
+	
+}); 
