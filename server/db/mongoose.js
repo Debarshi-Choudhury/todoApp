444604ec16a920b7e3be7c41d10fbc35c176ee43
Debarshi-Choudhury <debarshi.choudhury1@gmail.com>
2018-04-15 09:19:44 +0530

    connected to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 var mongoose=require('mongoose');
+var keys=require('../../config/keys');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(keys.mongodburi).then(()=>{
+   console.log("connected to mongodb");
+}).catch((e)=>{
+   console.log('could not connect to mongodb');
+});
 
 module.exports={mongoose};
+
+//'mongodb://localhost:27017/TodoApp'
+//
